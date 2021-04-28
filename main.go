066b53b02e18e92b49ec309b63cda558c7bc93da
Franco <francoivico@gmail.com>
2021-04-28 14:42:40 -0300

    Registro de usuario

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/francovico/twittor/bd"
+	"github.com/francovico/twittor/handlers"
+)
+
+func main() {
+	if bd.CheckConnection() == 0 {
+		log.Fatal("Sin conexion a la BD")
+		return
+	}
+	handlers.Manejadores()
 }
